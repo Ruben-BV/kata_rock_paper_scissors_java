213d3e6a01f37cfc0f27a6d6b193282f1936af65
Rubén Blanco <r.blanco.3d@gmail.com>
2024-12-01 00:39:30 +0100

    Implementada la clase Move Factory

---
+++ src/main/java/com/factoriaf5/rps/factory/MoveFactory.java
@@ -0,0 +1,27 @@
+package com.factoriaf5.rps.factory;
+
+import com.factoriaf5.rps.models.Lizard;
+import com.factoriaf5.rps.models.Move;
+import com.factoriaf5.rps.models.Paper;
+import com.factoriaf5.rps.models.Rock;
+import com.factoriaf5.rps.models.Scissors;
+import com.factoriaf5.rps.models.Spock;
+
+public class MoveFactory {
+    public Move createMove(int choice) {
+        switch (choice) {
+            case 1:
+                return new Rock("Rock");
+            case 2:
+                return new Paper("Paper");
+            case 3:
+                return new Scissors("Scissors");
+            case 4:
+                return new Lizard("Lizard");
+            case 5:
+                return new Spock("Spock");
+            default:
+                return null; 
+        }
+    }
+}
